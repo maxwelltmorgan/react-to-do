a1661910ebd97f51962803fcc09670a2fc4acba9
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-05-22 22:36:58 -0600

    checkbox

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,7 +4,10 @@ class ToDo extends Component {
   render () {
 
     return (
-      <li> A todo will go here </li>
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } />
+        <span>{ this.props.description }</span>
+      </li>
     );
   }
 }
